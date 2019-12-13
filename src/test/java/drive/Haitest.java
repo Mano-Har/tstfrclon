6623a64b0dd6613d3e194ddb80ff351857fae6a4
Mano2512 <manohar.s2512@gmail.com>
2019-12-13 10:28:13 +0530

    ordered by mas

--- src/test/java/drive/Haitest.java
+++ src/test/java/drive/Haitest.java
@@ -3,14 +3,17 @@ package drive;
 public class Haitest {
 
 	public void syso() {
-		System.out.println("Updated");
+		System.out.println(" branch 1  code ");
 
 		}	
+	public void hai1() {
+		System.out.println("branch 2 code");
+	}
+
 public static void main(String[] args) {
 	System.out.println("Branch2");
-}
-public void hai1() {
-}
+	
+          }
 
 	}
 	
